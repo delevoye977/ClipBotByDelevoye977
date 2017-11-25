0208cb709ca05a4cdad6e7bd54c0817a4db2f2ad
delevoye977 <super.val@live.fr>
2017-11-25 13:21:35 +0100

    Update PCMtoMP3.java

--- src/PCMtoMP3.java
+++ src/PCMtoMP3.java
@@ -5,11 +5,7 @@ import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 /**
  * Singleton class converts PCm files to WAV then MP3
  *
