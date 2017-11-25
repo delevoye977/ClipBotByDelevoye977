5626f9ee4ef0a15176053c2ac23d9b7dbbc987c4
delevoye977 <super.val@live.fr>
2017-11-25 13:21:24 +0100

    Update Writer.java

--- src/Writer.java
+++ src/Writer.java
@@ -5,11 +5,6 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  * Wrapper for FileOutputStream
  *
